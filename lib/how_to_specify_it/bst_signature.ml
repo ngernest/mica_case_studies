fe0692a70b9c56d62f1e7740fe8d3d977d25fb9c
Ernest Ng <eyn5@cornell.edu>
2024-10-06 11:40:43 -0400

    Add missing signature files for demo

---
+++ lib/how_to_specify_it/bst_signature.ml
@@ -0,0 +1,13 @@
+(** A module signature for binary search trees *)
+module type BST = sig
+  type ('k, 'v) t
+
+  val find : 'k -> ('k, 'v) t -> 'v option
+  val nil : ('k, 'v) t
+  val insert : 'k -> 'v -> ('k, 'v) t -> ('k, 'v) t
+  val delete : 'k -> ('k, 'v) t -> ('k, 'v) t
+  val union : ('k, 'v) t -> ('k, 'v) t -> ('k, 'v) t
+  val toList : ('k, 'v) t -> ('k * 'v) list
+  val keys : ('k, 'v) t -> 'k list
+  val size : ('k, 'v) t -> int
+end
